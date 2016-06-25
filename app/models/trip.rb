9cf0988f25152399d5baf0dbc6ef0437c2bfbb70
mieshajihan <mieshajw@gmail.com>
2016-06-24 19:40:27 -0500

    changed database file nm

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,20 +1,20 @@
 class Trip < ActiveRecord::Base
 
-field :orgin, :type => String
+field :orgin, :type: String
 
-field :desitination, :type => string
+field :desitination, type: String
 
-field :drop_off_time, :type => datetime
+field :drop_off_time, type: datetime
 
-field :pick_up_time, :type => datetime
+field :pick_up_time, type: datetime
 
-field :wheelchair, :type => boolean
+field :wheelchair, type: boolean
 
-field :phone_number, :type => string
+field :phone_number, type: string
 
-field :property_access_instructions, :type => text
+field :property_access_instructions, type: text
 
-field :special_instructions, :type => text
+field :special_instructions, type: text
 
 end
 
